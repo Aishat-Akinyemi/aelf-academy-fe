5914f252c9cd0a8947a2544f599a566a02392699
Aishat Adenike Akinyemi <aishatadenike01@gmail.com>
2022-06-15 02:41:02 +0000

    add ipfs methods

---
+++ src/utils/Ipfs.js
@@ -0,0 +1,32 @@
+import { create as ipfsHttpClient } from "ipfs-http-client";
+import axios from "axios";
+
+export const client = ipfsHttpClient("https://ipfs.infura.io:5001/api/v0");
+
+export async function uploadDataToIpfs(data) {
+    //uploads stringified data to ipfs
+    try {
+      // save to save data to IPFS
+      const added = await client.add(data);
+      // IPFS url for uploaded metadata
+      const url = `https://ipfs.io/ipfs/${added.path}`;
+  
+    } catch(error){
+        ipfs: 'https://ipfs.infura.io'
+    }
+    return url;
+}
+
+
+
+// get the data from IPFS
+export const fetchDataFromIpfs = async (ipfsUrl) => {
+    try {
+        if (!ipfsUrl) return null;
+        const data = await axios.get(ipfsUrl);
+        return data;
+    } catch (e) {
+        console.log({e});
+    }
+};
+
