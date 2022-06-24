dc65c341e66408d0a38b7733b56e522cbefe4ff9
Aishat Adenike Akinyemi <52966744+Aishat-Akinyemi@users.noreply.github.com>
2022-06-24 11:17:04 +0100

    add tooltip

--- src/components/Header.js
+++ src/components/Header.js
@@ -38,7 +38,7 @@ const Header = ({user, login}) => {
                 <a className="nav-link" href="https://discord.gg/Xag6TAPcgh" target="_blank" rel="noopener noreferrer">Discord</a>
             </li>
             <li className="nav-item">
-                <a className="nav-link btn" href="#">English</a>
+                <a className="nav-link btn" href="#"  title="multi-language Support coming soon">English</a>
             </li>
             <li className="nav-item">
              {   
