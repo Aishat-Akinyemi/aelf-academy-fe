1735fcf0682b0ba31ae57370833ec2bb9a067b91
Aishat Adenike Akinyemi <52966744+Aishat-Akinyemi@users.noreply.github.com>
2022-06-24 10:46:28 +0100

    temp fix to courses issue

--- src/Pages/Courses.js
+++ src/Pages/Courses.js
@@ -1,25 +1,61 @@
 import Course from "../components/Course";
-import { Row, Button } from 'react-bootstrap'
-import { useState, useEffect } from "react";
+import Loader from "../components/Loader";
+import { Row, Button } from 'react-bootstrap';
+import { useState, useEffect, useCallback } from "react";
 
 import { getAllCourses} from '../utils/Aelf';
+import { fetchDataFromIpfs } from "../utils/Ipfs";
 
 const Courses = ({user}) => {
     const [courses, setCourses] = useState([]);
-    const getCourses = async () => {
-        try{
-            const coursess = await getAllCourses();
-            setCourses(coursess);
-        } catch(e){
-            console.log(e);
-        } finally{
-        }
-    }
-
+    const [loading, setLoading] = useState(false);    
+    const [, updateState] = useState();
+    const getCourses = useCallback(() => {
+        setLoading(true);
+        getAllCourses().then(
+            (rawData)=>{
+                const coursesList = [];
+                rawData.map(course => {
+                    if(course.courseId==='1'){
+                        //correct error I committed during testing, TODO remember to remove after deploying a new contract
+                        course.contenturl = 'https://ipfs.io/ipfs/QmWeN3ttqoXJCPttynVBpZa3QE1e5TR9wuFqjPH6K99cSU';
+                    }
+                    fetchDataFromIpfs(course.contenturl).then(
+                        (res) => {
+                            const finalCourseObj= {
+                                courseId : course.courseId,
+                                submissionReward: course.submissionReward,
+                                moderationReward: course.moderationReward,
+                                level: course.level,
+                                introduction: res.data.introduction,
+                                toc: res.data.toc,
+                                challengeDescription: res.data.challengeDescription,
+                                content: res.data.challengeDescription,
+                                courseTitle: course.courseTitle ,
+                              }  
+                              if(course.courseId ==='3'){
+                                finalCourseObj.introduction = 'This course aims to help you setup your local system for smartcontract development on Aelf blockchain protocol.';
+                              } 
+                              return finalCourseObj;
+                        })
+                    .then(result => {
+                        coursesList.push(result);
+                        return result;
+                    })
+                    setCourses(coursesList);
+                    setLoading(false);
+                })
+              
+            },
+            (error) => {
+                alert(error.Message);
+                setLoading(false);
+            });    
+    });
+    const forceUpdate = useCallback(() => updateState({}), []);
     useEffect(() => {
         getCourses();
       }, []);
-
     
   return (
     <div>
@@ -28,25 +64,34 @@ const Courses = ({user}) => {
                 <h2 className="courses-page-title">About  Aelf academy courses</h2>
                 <p> Enrol in self-paced courses that teach development on Aelf blockchain in various levels from beginner to intermediate and advanced.</p>
                 <Button variant="outline-primary"
-                    onClick={getCourses}
+                    onClick={forceUpdate}
                 
                 >View All Courses</Button> 
             </header>
-
-           <div className="mlm mrm mbm pb-5">
-            <Row xs={1} sm={2} lg={3} className="g-3  mb-5 g-xl-4 g-xxl-5">
-                    {
-                        // courses.filter((course) => course.isActive===true)
-                        //     .map((course) => (
-                        //         <Course key={course.courseId} course={course} user={user}/>
-                        //     ))
-                        courses.map((course) => (
-                                <Course key={course.courseId} course={course} user={user}/>
-                        ))
+            {!loading ?
+                (<>
+                    {courses ?
+                        <div className="mlm mrm mbm pb-5">
+                            <Row xs={1} sm={2} lg={3} className="g-3  mb-5 g-xl-4 g-xxl-5">
+                                    {
+                                        // courses.filter((course) => course.isActive===true)
+                                        //     .map((course) => (
+                                        //         <Course key={course.courseId} course={course} user={user}/>
+                                        //     ))
+                                        courses.map((course) => (
+                                                <Course key={course.courseId} course={course} user={user}/>
+                                        ))
+                                    }
+                                </Row>
+                        </div>
+                        : <div className="mlm mrm mbm pb-5"><p>No courses Yet</p></div>
+                        }
+                    </>):
+                     (<Loader/>)
                     }
-                </Row>
-           </div>
+                
 
+            
         
         </div>
     </div>
@@ -54,3 +99,5 @@ const Courses = ({user}) => {
 }
 
 export default Courses
+
+
